0673248ca1770781885a9ccc7154f5b9fd071875
Gil Cunha <gilcunha@outlook.pt>
2022-11-17 20:32:12 +0000

    inicial configs

--- HelloWorld.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const server = http.createServer(function(request, respose){
-    respose.writeHead(200, {'Content-Type':'text/html'});
-    respose.write('<html><body><h1>Olá, Mundo</h1></body></html>');
-    respose.end();
-});
-server.listen();
-console.log('Servidor Node.js em execução');
